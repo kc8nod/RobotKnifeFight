e60dd6fd577be66459fcecb3a94c62e778b3ffaf
Matthew G Switlik <switlikm@gmail.com>
2012-09-17 20:20:21 -0400

    BT DRIVABLE!!!!!

--- code/bt_drivable_bot/bt_drivable_bot.ino
+++ code/bt_drivable_bot/bt_drivable_bot.ino
@@ -6,12 +6,42 @@
 
 int incomingByte;
 
+unsigned long now = 0;
+
+ServoTimer2 LeftDrive, RightDrive;
+#define LEFT_FWD 2000
+#define LEFT_STOP 1500
+#define LEFT_REV 1000
+#define RIGHT_FWD 1000
+#define RIGHT_STOP 1500
+#define RIGHT_REV 2000
+unsigned int speed_L = LEFT_STOP;
+unsigned int speed_R = RIGHT_STOP;
+unsigned long timeLastServoUp = 0;
+
+RKF_Radio radio;
+
 void setup() {
   Serial.begin(115200);
+  
+  LeftDrive.attach(LEFT_DRIVE_PIN);
+  RightDrive.attach(RIGHT_DRIVE_PIN);
+  
+  pinMode(BUMP_SWITCH_RIGHT_PIN, INPUT);
+  pinMode(BUMP_SWITCH_LEFT_PIN, INPUT);
+  pinMode(DEFAULT_RX_PIN, INPUT);
   pinMode(STATUS_LED_PIN, OUTPUT);
+  
+  LeftDrive.write(LEFT_STOP);
+  RightDrive.write(RIGHT_STOP);
+  
+  radio.start();
 }
 
 void loop() {
+  
+  now = millis();
+  
   if (Serial.available() > 0) {
     incomingByte = Serial.read();
     switch(incomingByte) {
@@ -19,25 +49,44 @@ void loop() {
       case 'W':
         digitalWrite(STATUS_LED_PIN, HIGH);
         Serial.print("w");
+        speed_L = LEFT_FWD;
+        speed_R = RIGHT_FWD;
         break;
       case 'a':
       case 'A':
         digitalWrite(STATUS_LED_PIN, HIGH);
         Serial.print('a');
+        speed_L = LEFT_REV;
+        speed_R = RIGHT_FWD;
         break;
       case 's':
       case 'S':
         digitalWrite(STATUS_LED_PIN, HIGH);
         Serial.print("s");
+        speed_L = LEFT_REV;
+        speed_R = RIGHT_REV;
         break;
       case 'd':
       case 'D':
         digitalWrite(STATUS_LED_PIN, HIGH);
         Serial.print("d");
+        speed_L = LEFT_FWD;
+        speed_R = RIGHT_REV;
         break;
       default:
         digitalWrite(STATUS_LED_PIN, LOW);
+        speed_L = LEFT_STOP;
+        speed_R = RIGHT_STOP;
         break;
     }
   }
+  
+  //Update Servo positions  
+  if(now - timeLastServoUp > 15){  //limit servo updating to every 15ms at most
+    LeftDrive.write(speed_L);
+    RightDrive.write(speed_R);
+    timeLastServoUp = now;
+  }
+  
+  delay(1);
 }
