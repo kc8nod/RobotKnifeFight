80e4f5bb76a45e6365c169345815a4ac3177bc9a
swit <swit@RobotFightClub.(none)>
2013-04-26 19:04:26 -0400

    multi qr code positions works! field of view needs improvement

--- code/arena/rfc_qr_arena.py
+++ code/arena/rfc_qr_arena.py
@@ -1,64 +1,32 @@
 import cv, cv2
 import sys, math, time
 from numpy import *
+import re
+import zbar
+import Image
 
 ###############
 ## FUNCTIONS
 ###############
 
-def onMouse (event, x, y, flags, param):
-    #print event, x, y, flags, param
-    global objectsPts
-    global objectPntIndex
-    global outputImg
-    global pointer
-    
-    if event == cv2.EVENT_LBUTTONUP:
-        #print "LBUTTONUP", x, y, flags, param
-        if drawing:
-            s = objectsPts.shape
-            objectsPts.itemset((objectIndex,objectPntIndex,0), x)
-            objectsPts.itemset((objectIndex,objectPntIndex,1), y)
-            objectPntIndex += 1
-            if objectPntIndex >= s[1]:
-                objectPntIndex = 0
-            print "Added Point:",objectIndex,objectPntIndex,x,y
-            
-    if event == cv2.EVENT_MOUSEMOVE:
-        pointer = (x, y)
-
 def displayMenu ():
     print ""
     print "------------------------------"
     print "'h' Print this help menu."
-    print "'n' Increase the number of robots to scan for"
     print "'f' 'g' Changes focus"
     print "'v' Toggle output view."
     print "'s' Display statuses."
     print "'Space bar' to pause/play video playback."
     print "'Esc' to exit."
-    print "------------------------------"
-
 
 def displayStatuses ():
     print ""
     print "------------------------------"
-    print "NumOfRobots:",numberOfRobots
     print "Focus:",focus
-    print "------------------------------"
-
 
 def displayPrint(a, b="", c=""):
     print "------------------------------"
     print a, b, c
-    print "------------------------------"
-
-
-def increaseNumOfRobots():
-    global numberOfRobots
-    numberOfRobots += 1
-    if numberOfRobots > 4:
-        numberOfRobots = 1
 
 def increaseFocus():
     global focus, cap
@@ -87,40 +55,56 @@ CV_CAP_PROP_FRAME_WIDTH = 3
 CV_CAP_PROP_FRAME_HEIGHT = 4
 CV_CAP_PROP_FOCUS = 28
 
-focus = 1
+index_pattern = re.compile('\d+')
+cameraindex = 0
+focus = 2
 
 playFromFile = False
 loopVideo = False            
 if len(sys.argv) > 1:
-  playFromFile = True
-  cap = cv2.VideoCapture(sys.argv[1])
-  if len(sys.argv) > 2:
-    if sys.argv[2] == "-l":
-      loopVideo = True
+    playFromFile = True
+    cap = cv2.VideoCapture(sys.argv[1])
+    if len(sys.argv) > 2:
+        if sys.argv[2] == "-l":
+            loopVideo = True
 else:
-  cap = cv2.VideoCapture(0)
-  cap.set(CV_CAP_PROP_FRAME_WIDTH, 1920)
-  cap.set(CV_CAP_PROP_FRAME_HEIGHT, 1080)
-  cap.set(CV_CAP_PROP_FOCUS, focus)
+    print
+    print "Enter the camera's index"
+    in_str = raw_input("defaults [%1d] " % cameraindex)
+    match = index_pattern.match(in_str)
+    if match:
+        print match.group(0)
+        cameraindex = int(match.group(0))
+    else:
+        print "using defaults"
+        
+    cap = cv2.VideoCapture(cameraindex)
+    cap.set(CV_CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT, 720)
+    cap.set(CV_CAP_PROP_FOCUS, focus)
+  
 cv2.namedWindow("ArenaScanner")
+
+cv2.startWindowThread()
+
 key = -1
 
 FPS = 30 # For video file playback only. 30 seems to match actual robot speed.
 paused = False
-drawing = False 
+
 dataview = 1
 
-numberOfRobots = 1
-
-pointer = (0,0)
-
-cv2.setMouseCallback("ArenaScanner", onMouse)
 success, prevImg = cap.read()
 
 displayMenu()
 displayStatuses()
 
 prevTime = time.time()
+
+scanner = zbar.ImageScanner()
+#scanner.parse_config('enable')
+scanner.set_config(0, zbar.Config.ENABLE, 0) #disable all symbols
+scanner.set_config(zbar.Symbol.QRCODE, zbar.Config.ENABLE, 1) #enable QR codes
 
 ###############
 ## LOOP
@@ -134,45 +118,51 @@ while True:
     if playFromFile == False or paused == False:
         #get next frame from capture device
         success, nextImg = cap.read()
-        lastImg = nextImg
+        prevImg = nextImg
         if not success:
             if loopVideo:
                 cap = cv2.VideoCapture(sys.argv[1])
                 continue
             break;
     else:
-        nextImg = lastImg
+        nextImg = prevImglastImg
 
 
     #Apply image transformations
     if dataview == 0:
         outputImg = nextImg.copy()
     nextImg = cv2.cvtColor(nextImg, cv2.COLOR_BGR2GRAY)
-    #nextImg = cv2.dilate(nextImg, None, iterations=1)
-    #retval,nextImg = cv2.threshold(nextImg, 100, 255, cv2.THRESH_BINARY)
     if dataview == 1:
         outputImg = cv2.cvtColor(nextImg, cv2.COLOR_GRAY2BGR)
         
-        
+    #Scan for QR codes
+    width = size(nextImg, 1)
+    height = size(nextImg, 0)
+    #print width, height
+    raw = nextImg.tostring()
+    image = zbar.Image(width, height, 'Y800', raw)
+    scanner.scan(image)
+    for symbol in image:
+        # do something useful with results
+        print symbol.type, '"%s"' % symbol.data, ' location',symbol.location
+
+
+
     #Draw output        
     cv2.imshow("ArenaScanner", outputImg)
 
     
     #process key presses        
-    key = cv2.waitKey(1)        
-    if key == 27:  #esc
+    key = cv2.waitKey(10)        
+    if key == 27 or key==1048603:  #esc
         break #exit
 
-    if key == 32: #spacebar
+    elif key == 32: #spacebar
         paused = not paused
       
     elif key == 104: #h key
         displayMenu()
         
-    elif key == 110: #n key
-        increaseNumOfRobots()
-        displayStatuses()
-
     elif key == 115: #s key
         displayStatuses()
 
@@ -180,11 +170,11 @@ while True:
         decreaseFocus()
         displayStatuses()
 
-    elif key == 103: #g key
+    elif key == 103 or key==1048679: #g key
         increaseFocus()
         displayStatuses()
     
-    elif key == 118: #v key
+    elif key == 118 or key==1048694: #v key
         dataview += 1
         if dataview > 1:
             dataview = 0
@@ -194,7 +184,10 @@ while True:
         displayPrint("unknown key",key)
 
     prevImg = nextImg.copy()
-print "Exiting."    
 
 cap.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()        
+print "Exiting."    
+
+
+
